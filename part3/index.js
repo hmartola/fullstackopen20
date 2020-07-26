1e36e48695ca285a2d500dcdce816e050a0e3c49
hmartola <64286599+hmartola@users.noreply.github.com>
2020-07-26 22:20:38 +0300

    changes for heroku

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
     {
@@ -83,7 +85,7 @@ app.delete('/api/persons/:id', function (req, res) {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
