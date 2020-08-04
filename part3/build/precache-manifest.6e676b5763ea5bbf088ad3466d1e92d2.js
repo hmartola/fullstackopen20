d9a82404fd223b1814e33174a2a07cd6c7bebbc4
hmartola <64286599+hmartola@users.noreply.github.com>
2020-08-04 19:45:34 +0300

    phonebook build dir

---
+++ part3/build/precache-manifest.6e676b5763ea5bbf088ad3466d1e92d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fcbae66a274151d8fb7765c95b724c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91b3e5d1f2bcfc894c5",
+    "url": "/static/css/main.f99004b6.chunk.css"
+  },
+  {
+    "revision": "d5fbea774a0e3a43449c",
+    "url": "/static/js/2.52ac1312.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52ac1312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91b3e5d1f2bcfc894c5",
+    "url": "/static/js/main.d1cbcebe.chunk.js"
+  },
+  {
+    "revision": "dc11ffb1a1fcdaebfb6e",
+    "url": "/static/js/runtime-main.15a3d875.js"
+  }
+]);
