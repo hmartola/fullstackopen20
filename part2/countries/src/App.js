12cd7977f15e4463e5d921727048b1fc1baa7d26
hmartola <64286599+hmartola@users.noreply.github.com>
2020-09-28 17:57:01 +0300

    exercies 2.12-2.14 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,34 +1,45 @@
 import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import axios from 'axios'
+import Countries from './components/Countries'
 
 function App() {
 
   const [data, setData] = useState([])
   const [search, setSearch] = useState('')
 
-  const hook = () => {
-    console.log("effect")
-  axios
-    .get('https://restcountries.eu/rest/v2/all')
+
+  useEffect(() => {
+  axios.get('https://restcountries.eu/rest/v2/all')
     .then(response => {
       console.log("promise fulfilled")
+      setData(response.data) 
+    })
+  }, [])
+
+  
+  const clickedShow = (view) => {
+    axios
+    .get(`https://restcountries.eu/rest/v2/name/${view}`)
+    .then(response => {
       setData(response.data)
     })
   }
-
-  useEffect(hook, [])
-
+  
   const handleSearchEdit = (event) => {
+    event.preventDefault()
     setSearch(event.target.value)
-  } 
+  }
+  
+   const filter = data.filter(country => 
+    country.name.toLowerCase().includes(search)) 
+    
 
   return (
     <div>
       <h1>Country Data</h1>
       find countries: <input value={search} onChange={handleSearchEdit}/>
-      
+      <Countries country={filter} view={clickedShow}/>
     </div>
   );
 }
---
+++ part2/countries/src/components/Countries.js
@@ -0,0 +1,78 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+const Weather = ({capital}) => {
+    const [weather, setWeather] = useState([])
+    const [main, setMain] = useState([])
+    const [wind, setWind] = useState([])
+    const key = process.env.REACT_APP_API_KEY
+
+    const hook = () => {
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${key}`)
+    .then(res => {
+        setWeather(res.data.weather)
+        setMain(res.data.main)
+        setWind(res.data.wind)
+    })
+    }
+    useEffect(hook, [])
+
+    return (
+        <div>
+            {weather.map(w => (<div key={w.id}><p><b>{w.main}</b> ({w.description})</p></div>))}
+            <p><b>temperature:</b> {main.temp} celsius</p>
+            <p><b>feels like:</b> {main.feels_like} celsius</p>
+            <p><b>wind: </b> {wind.speed} m/s</p>
+        </div>
+    )
+}
+
+const CountryData = ({country}) => {
+
+    return (
+        <div>
+            <h2>{country.name}</h2>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+            <h3>languages</h3>
+            <ul>{country.languages.map(lang => (<li key={lang.name}>{lang.name}</li>))}</ul>
+            <img src={country.flag} alt="" width="140" height="100"></img>
+            <h3>Weather in {country.capital}</h3>
+            <Weather capital={country.capital}></Weather>
+        </div>
+    )
+}
+
+const Countries = ({country, view}) => {
+    if (country.length > 10) {
+        return (
+            <div>
+            <p>Too many matches, specify new filter</p>
+            </div>
+        )
+    }
+    if (country.length === 1) {
+        return (
+            <div>
+                {country.map(c => 
+                <CountryData key={c.name} country={c}></CountryData>
+                )}
+            </div>
+        )
+    } 
+
+    return (
+        <div>
+            {country.map(c => 
+                <div key={c.name}>
+                        {c.name}
+                        <button onClick={() => view(c.name)}>show</button>
+                </div>
+            )}
+        </div>
+    )
+
+}
+
+export default Countries
